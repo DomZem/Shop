fdfb5f3b7c92553f88f9b8ae570107e96ff49040
DomZem <dominik.zemlik.dev@gmail.com>
2024-05-23 21:23:13 +0200

    feat: create main entities

---
+++ Shop.Domain/Entities/Order.cs
@@ -0,0 +1,19 @@
+﻿namespace Shop.Domain.Entities
+{
+    public class Order
+    {
+        public int Id { get; set; }
+
+        public DateTime OrderedAt { get; set; } = DateTime.UtcNow;
+
+        public int ProductQuantity { get; set; }
+
+        public int ProductId { get; set; }
+
+        public Product Product { get; set; } = default!;
+
+        public int OrderStatusId { get; set; }
+
+        public OrderStatus OrderStatus { get; set; } = default!;
+    }
+}
