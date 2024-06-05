13feee1ffda7e2b73a80e7df594edd65e65e7000
DomZem <dominik.zemlik.dev@gmail.com>
2024-06-05 19:19:59 +0200

    feat(grpc): change styles for table

--- Shop.GrpcService/Configurations/HtmlToPdfConfiguration.cs
+++ Shop.GrpcService/Configurations/HtmlToPdfConfiguration.cs
@@ -16,8 +16,8 @@ namespace Shop.GrpcService.Configurations
         {
             PagesCount = true,
             WebSettings = { DefaultEncoding = "utf-8", UserStyleSheet = Path.Combine(Directory.GetCurrentDirectory(), "assets", "styles.css") },
-            HeaderSettings = { FontName = "Arial", FontSize = 9, Right = "Page [page] of [toPage]", Line = true },
-            FooterSettings = { FontName = "Arial", FontSize = 9, Line = true, Center = "Report Footer" }
+            HeaderSettings = { Right = "Page [page] of [toPage]", Line = true },
+            FooterSettings = { Line = true, Center = "Report Footer" }
         };
     }
 }
