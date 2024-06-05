17f2b9d218c10e1d999956272fb586d65f319f82
DomZem <dominik.zemlik.dev@gmail.com>
2024-06-05 18:45:57 +0200

    fix: fix issue with not able login to app after seed

--- Shop.Infrastructure/Seeders/ShopSeeder.cs
+++ Shop.Infrastructure/Seeders/ShopSeeder.cs
@@ -28,6 +28,20 @@ namespace Shop.Infrastructure.Seeders
                 {
                     var users = GetUsers();
                     dbContext.Users.AddRange(users);
+
+                    // assign roles to users
+                    dbContext.UserRoles.AddRange(
+                        new IdentityUserRole<string>()
+                        {
+                            UserId = "1481d8da-89e3-4451-a2ff-02ab11249679",
+                            RoleId = "cad52c90-c333-4e8e-8047-fb1f13c069e1",
+                        },
+                        new IdentityUserRole<string>()
+                        {
+                            UserId = "2be03688-814d-45c9-8821-31a00f181cea",
+                            RoleId = "34b6e195-78c6-4aba-b3c0-da41435ce3ad",
+                        }
+                    );
                     await dbContext.SaveChangesAsync();
                 }
 
@@ -51,15 +65,17 @@ namespace Shop.Infrastructure.Seeders
             [
                 new IdentityRole(UserRoles.Admin) 
                 {
+                    Id = "cad52c90-c333-4e8e-8047-fb1f13c069e1",
                     NormalizedName = UserRoles.Admin.ToUpper(),
                 },
                 new IdentityRole(UserRoles.User)
                 {
+                    Id = "34b6e195-78c6-4aba-b3c0-da41435ce3ad",
                     NormalizedName = UserRoles.User.ToUpper(),
-
                 },
                 new IdentityRole(UserRoles.Owner)
                 {
+                    Id = "e0358f57-c10b-4d43-b313-e76f27c60875",
                     NormalizedName = UserRoles.Owner.ToUpper(),
                 }
             ];
@@ -71,29 +87,37 @@ namespace Shop.Infrastructure.Seeders
         {
             var admin = new User
             {
+                Id = "1481d8da-89e3-4451-a2ff-02ab11249679",
                 Email = "admin@gmail.com",
                 EmailConfirmed = true,
                 UserName = "admin",
-                PhoneNumber = "+48 111 222 333",
-                NormalizedUserName = "ADMIN",
-            };
-
-            var user = new User
-            {
-                Email = "kylianmbapper@gmail.com",
-                EmailConfirmed = true,
-                UserName = "kylian",
-                PhoneNumber = "+48 444 555 666",
-                NormalizedEmail = "KYLIAN"
             };
 
             // Hash admin password
             PasswordHasher<User> adminPh = new PasswordHasher<User>();
             admin.PasswordHash = adminPh.HashPassword(admin, "zaq1@WSX");
-                
+
+            admin.NormalizedUserName = admin.Email.ToUpper();
+            admin.NormalizedEmail = admin.Email.ToUpper();
+            admin.LockoutEnabled = true;
+            admin.LockoutEnd = null;
+
+            var user = new User
+            {
+                Id = "2be03688-814d-45c9-8821-31a00f181cea",
+                Email = "kylianmbappe@gmail.com",
+                EmailConfirmed = true,
+                UserName = "Kylain",
+            };
+
             // Hash user password
             PasswordHasher<User> userPh = new PasswordHasher<User>();
             user.PasswordHash = userPh.HashPassword(user, "zaq1@WSX");
+
+            user.NormalizedUserName = user.Email.ToUpper();
+            user.NormalizedEmail = user.Email.ToUpper();
+            user.LockoutEnabled = true;
+            user.LockoutEnd = null;
 
             List<User> users = [admin, user];
 
@@ -102,7 +126,7 @@ namespace Shop.Infrastructure.Seeders
 
         private IEnumerable<Order> GetOrders()
         {
-            var user = dbContext.Users.Where(u => u.Email == "kylianmbapper@gmail.com").ToList().First();
+            var user = dbContext.Users.Where(u => u.Email == "kylianmbappe@gmail.com").ToList().First();
 
             if (user == null)
             {
@@ -116,6 +140,7 @@ namespace Shop.Infrastructure.Seeders
                     ProductQuantity = 1,
                     OrderStatus = new OrderStatus() { Name = "Preparation" },
                     OrderedById = user.Id,
+                    TotalPrice = 1 * 699.99M,
                     OrderAddress = new OrderAddress()
                     {
                         Street = "456 Oak Avenue", 
@@ -137,6 +162,7 @@ namespace Shop.Infrastructure.Seeders
                     ProductQuantity = 2, 
                     OrderStatus = new OrderStatus() { Name = "Shipped" },
                     OrderedById = user.Id,
+                    TotalPrice = 2 * 24.99M,
                     OrderAddress = new OrderAddress()
                     {
                         Street = "789 Pine Lane", 
@@ -158,6 +184,7 @@ namespace Shop.Infrastructure.Seeders
                     ProductQuantity = 3,
                     OrderStatus = new OrderStatus() { Name = "Delivered" },
                     OrderedById = user.Id,
+                    TotalPrice = 3 * 9.99M,
                     OrderAddress = new OrderAddress()
                     {
                         Street = "123 Main Street", 
